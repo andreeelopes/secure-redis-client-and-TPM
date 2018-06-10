0e7932dc8766b67586142aee67d4bf4351ba5214
Simao <s.dolores@campus.fct.unl.pt>
2018-06-10 12:47:36 -0400

    Add Benchmark

--- SRSC-P2/src/main/java/GetSetBenchmark.java
+++ SRSC-P2/src/main/java/GetSetBenchmark.java
@@ -1,55 +1,38 @@
 package main.java;
 
+
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.Calendar;
 
-import TPM.client.TPMClient;
-import redis.client.RedisClient;
-
+import redis.clients.jedis.Jedis;
 
 public class GetSetBenchmark {
-	private static final int TOTAL_OPERATIONS = 100;
+    private static final int TOTAL_OPERATIONS = 1000;
 
-	public static void main(String[] args) throws UnknownHostException, IOException {
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Jedis jedis = new Jedis("localhost", 6379);
 
-		long begin = Calendar.getInstance().getTimeInMillis();
+        jedis.connect();
+        jedis.flushAll();
 
-		//if attestion ok do benchmark
-		if(	new TPMClient("TPMClientConfig.json").attest("localhost", 4446, "localhost", 4443)) {
+        long begin = Calendar.getInstance().getTimeInMillis();
 
-			//Jedis jedis = new Jedis("rediss://localhost", 6379);
-			RedisClient jedis = new RedisClient();
+        for (int n = 0; n <= TOTAL_OPERATIONS; n++) {
+            String key = "11111111111111111111111111111111:11111111111111111111111111111111:11111111111111111111111111111111" + n;
+            jedis.set(key, "22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222" + n);
+            // System.out.println(jedis.get(key));
+        }
+        for (int n = 0; n <= TOTAL_OPERATIONS; n++) {
+            String key = "11111111111111111111111111111111:11111111111111111111111111111111:11111111111111111111111111111111" + n;
+            jedis.get(key);
+            //System.out.println(jedis.get(key));
+        }
 
-			//jedis.connect();
-		
+        long elapsed = Calendar.getInstance().getTimeInMillis() - begin;
 
+        jedis.disconnect();
 
-			for (int n = 0; n <= TOTAL_OPERATIONS; n++) {
-
-				jedis.setClient(n,"test", "test", "test", "test", "test");
-				// System.out.println(jedis.get(key));
-			}
-			for (int n = 0; n <= TOTAL_OPERATIONS; n++) {
-
-				jedis.getClientID("n");
-				//System.out.println(jedis.get(key));
-			}
-
-			long elapsed = Calendar.getInstance().getTimeInMillis() - begin;
-
-		
-
-			System.out.println(((1000 * 2 * TOTAL_OPERATIONS) / elapsed) + " ops/s");	
-		}
-
-
-	}
+        System.out.println(((1000 * 2 * TOTAL_OPERATIONS) / elapsed) + " ops/s");
+    }
 }
-
-
-
-
-
-
-
---
+++ SRSC-P2/src/main/java/SafeGetSetBenchmark.java
@@ -0,0 +1,55 @@
+package main.java;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Calendar;
+
+import TPM.client.TPMClient;
+import redis.client.RedisClient;
+
+
+public class SafeGetSetBenchmark {
+	private static final int TOTAL_OPERATIONS = 100;
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+
+		long begin = Calendar.getInstance().getTimeInMillis();
+
+		//if attestion ok do benchmark
+		if(	new TPMClient("TPMClientConfig.json").attest("localhost", 4446, "localhost", 4443)) {
+
+			//Jedis jedis = new Jedis("rediss://localhost", 6379);
+			RedisClient jedis = new RedisClient();
+
+			//jedis.connect();
+		
+
+
+			for (int n = 0; n <= TOTAL_OPERATIONS; n++) {
+
+				jedis.setClient(n,"test", "test", "test", "test", "test");
+				// System.out.println(jedis.get(key));
+			}
+			for (int n = 0; n <= TOTAL_OPERATIONS; n++) {
+
+				jedis.getClientID("n");
+				//System.out.println(jedis.get(key));
+			}
+
+			long elapsed = Calendar.getInstance().getTimeInMillis() - begin;
+
+		
+
+			System.out.println(((1000 * 2 * TOTAL_OPERATIONS) / elapsed) + " ops/s");	
+		}
+
+
+	}
+}
+
+
+
+
+
+
+
