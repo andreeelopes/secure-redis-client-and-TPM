4a9baaf3471ccf1db68a70509eed2144541b9d33
Coquenim <nelson_coquenim@hotmail.com>
2018-06-12 20:56:01 +0100

    Updated jars for both of the TPM modules.

--- SRSC-P2/src/TPM/server/VMSTPMServer.java
+++ SRSC-P2/src/TPM/server/VMSTPMServer.java
@@ -2,7 +2,7 @@ package TPM.server;
 
 public class VMSTPMServer extends TPMServer{
 
-
+	
 	public VMSTPMServer(int port, String configFilePath, String attestCommandsPath) {
 		super(port, configFilePath, attestCommandsPath);
 		super.initiateAttestationProtocol();
